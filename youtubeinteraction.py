fc1e5af2f4531c792620402b714416ad0c0df569
Bryce David Yoder <bdyoder@va22-imac01.gc.goshen.edu>
2018-11-15 19:26:36 -0500

    Rank links and open in default browser

--- youtubeinteraction.py
+++ youtubeinteraction.py
@@ -1,12 +1,38 @@
 from bs4 import BeautifulSoup
 import requests
+import webbrowser
 
-title = "Riptide"
-artist = "Vance Joy"
+title = "Numb"
+artist = "OJR"
+
+
+def rank_results(result_list, search_title, search_artist):
+
+    scores = []
+
+    for title in result_list:
+        score = 0;
+        if search_title in title:
+            score += 1
+        if search_artist in title:
+            score +=1
+        if search_title in title and search_artist in title:
+            score += 1
+        if title == search_title:
+            score = 100
+        scores.append(score)
+
+    return scores.index(max(scores))
+
+
+
+
 
 def scrape(search_title, search_artist):
-    artist = search_artist.replace(" ","+")
-    search_query = search_title + "+" + artist
+    search_artist = search_artist.replace(" ","+")
+    search_title = search_title.replace(" ","+")
+
+    search_query = search_title + "+" + search_artist
     youtube_url = "https://www.youtube.com/results?search_query=" + search_query
     header = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 
@@ -25,26 +51,10 @@ def scrape(search_title, search_artist):
         title.append(h3.find('a').contents[0])
         ref.append(h3.find('a')['href'])
 
-    sequence = ["S.No", "Title"]
-    t = PrettyTable(sequence)
-    sz = len(title)
-    print("Results found = " + str(sz))
-    sys.setrecursionlimit(100000)
-    for i in range(sz):
-        t.add_row([i + 1, title[i]])
-    if len(title) != 0:
-        print(color.BOLD + color.CYAN + "\nResults : " + color.END)
-        print(t)
+    best_title = rank_results(title, search_title, search_artist)
+    print("Best result is: '"+title[best_title]+"' at index "+str(best_title))
 
-        choice = input(color.CYAN + color.BOLD + "\nEnter your choice (numerical) : " + color.END)
-        if 1 <= int(choice) <= len(title):
-            filename = title[int(choice) - 1]
-            video_url = "https://www.youtube.com" + str(ref[int(choice) - 1])
-            return filename, video_url
-        else:
-            print("Invalid entry.")
-            sys.exit()
-    else:
-print(color.BOLD + color.CYAN + "Sorry, no results found." + color.END)
+    webbrowser.open('https://www.youtube.com'+ref[best_title])
 
 
+scrape(title, artist)
